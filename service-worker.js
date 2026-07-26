cb0a36aaea6be0272200393dd2406dade6462389
mikeindia1982 <115977281+mikeindia1982@users.noreply.github.com>
2026-07-26 21:28:01 +0200

    0.9.3.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='abwasser-rechner-v0.9.2a';
-const FILES=["./","./index.html","./styles.css?v=0.9.2a","./manifest.webmanifest","./icon-192.png","./icon-512.png","./js/app.js?v=0.9.2a","./js/qrcode-offline.js","./js/utils.js","./js/calculators.js","./js/chemistry.js","./js/dewatering.js","./plant-hero-base.png"];
+const CACHE='abwasser-rechner-v0.9.3';
+const FILES=["./","./index.html","./styles.css?v=0.9.3","./manifest.webmanifest","./icon-192.png","./icon-512.png","./js/app.js?v=0.9.3","./js/qrcode-offline.js","./js/utils.js","./js/calculators.js","./js/chemistry.js","./js/dewatering.js","./plant-hero-base.png"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)))});
 self.addEventListener("activate",event=>event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))])));
 self.addEventListener("fetch",event=>{
