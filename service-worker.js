70904819d2c744d7105858caa915be8f327911d2
mikeindia1982 <115977281+mikeindia1982@users.noreply.github.com>
2026-08-16 23:11:47 +0200

    Feature: Dokument-Prüfassistent offline cachen

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,15 @@
-const CACHE='abwasser-rechner-v0.11.0-alpha.22';
+const CACHE='abwasser-rechner-v0.11.0-alpha.23';
 const FILES=[
   "./",
   "./index.html",
   "./styles.css?v=0.11.0-alpha.19",
-  "./document-fixes.css?v=0.11.0-alpha.21",
+  "./document-fixes.css?v=0.11.0-alpha.23",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png",
   "./js/app.js?v=0.11.0-alpha.19",
   "./js/sidebar-tasks.js?v=0.11.0-alpha.19",
+  "./js/document-review-enhancements.js?v=0.11.0-alpha.23",
   "./js/components/product-image.js",
   "./js/db/database.js",
   "./js/repositories/document-repository.js",
