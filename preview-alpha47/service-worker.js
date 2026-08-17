e0b764491a000759664d171f53f13f4ce4a2dbdf
mikeindia1982 <115977281+mikeindia1982@users.noreply.github.com>
2026-08-17 23:48:01 +0200

    chore: cache commercial intelligence in preview

--- preview-alpha47/service-worker.js
+++ preview-alpha47/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='abwasser-preview-alpha47-';
-const CACHE=`${CACHE_PREFIX}v5`;
+const CACHE=`${CACHE_PREFIX}v6`;
 const FILES=[
   "./",
   "./index.html",
@@ -32,6 +32,7 @@ const FILES=[
   "./js/navigation-enhancements.js?v=0.11.0-alpha.38",
   "./js/app.js?v=0.11.0-alpha.19",
   "../js/open-map-provider.js?v=0.11.0-alpha.55",
+  "../js/customer-supply-intelligence.js?v=0.11.0-alpha.56",
   "./js/preview-task-mirror.js?v=preview-alpha47-taskfix1",
   "./js/startup-dashboard-guard.js?v=0.11.0-alpha.37",
   "./js/sidebar-tasks.js?v=0.11.0-alpha.39",
