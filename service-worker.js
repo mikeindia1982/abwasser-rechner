5db798ba3a1fa8f00d06a8e270117bccadc6d6e5
mikeindia1982 <115977281+mikeindia1982@users.noreply.github.com>
2026-08-22 16:11:47 +0200

    chore: refresh PWA cache after IndexedDB migration

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='abwasser-rechner-';
-const CACHE=`${CACHE_PREFIX}v0.11.0-alpha.78-knowledge-content3`;
+const CACHE=`${CACHE_PREFIX}v0.11.0-alpha.78-knowledge-content4`;
 const FILES=[
   "./",
   "./index.html",
